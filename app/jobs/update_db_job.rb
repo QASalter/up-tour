76eede1b0c134edf41811eed56308d66451b8368
Sam Salter <s.salter@livelinktechnology.net>
2018-03-04 18:38:13 +0000

    fix repro bug

--- app/jobs/update_db_job.rb
+++ app/jobs/update_db_job.rb
@@ -2,9 +2,9 @@ class UpdateDbJob < ApplicationJob
   queue_as :default
 
   def perform
-    TimeCheck.where(id: 1).first_or_create do |time|
-      time.last_checked = Time.now
-    end
+    time = TimeCheck.find(1)
+    time.update!(last_checked: Time.now)
+
     ApplicationHelper.get_csv
     Club.update_clubs
     Team.update_team
